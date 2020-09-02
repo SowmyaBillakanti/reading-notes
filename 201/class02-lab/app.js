7bef9d27acd67c7fc14e06c59cd08deaeb1bedb5
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-09-01 18:53:11 -0700

    Styled my Guessing game page

--- 201/class02-lab/app.js
+++ 201/class02-lab/app.js
@@ -14,49 +14,58 @@ console.log('JS is loaded');
 var user = prompt('Am I your friend?');
 if (user.toLowerCase() === 'y'|| user.toLowerCase() === 'yes'){
     alert("She is a perfect friend to be with!");
+    // console.log("Yes, You are correct!");
 
 }else {
-alert("Lets be friends");
+alert("We are friends!!");
+// console.log("No, You are wrong :(");
+
 }
     console.log(user);
-    console.log(user = "She will be a great friend for you");
 
     var animal = prompt('Is she an Elephant lover?');
     if (animal.toLowerCase() === 'y'|| animal.toLowerCase() === 'yes'){
     alert("Yes!Elephants are adorable");
+    // console.log("Yes, You are correct!");
     } else {
         alert('Oops! We should meet more often to know me better');
+        // console.log("No, You are wrong :(");
     }
-    console.log('animal');
-    console.log(animal = 'Elephants are cute');
+    console.log(animal);
 
-    var place = prompt("Is she from India?");
+
+    var place = prompt("Is she from United States?");
     if (place.toLowerCase() === 'y'|| place.toLowerCase() === 'yes'){
-        alert('Awesome! You are correct');
+        alert('No! She is from India');
+        // console.log("Yes, You are correct!");
     } else {
-        alert('Lets plan a vacation to India');
+        alert('Yay! You are correct');
+        // console.log("No, You are wrong :(");
     }
 
     console.log(place);
-    console.log(place = 'Land of spices');
 
-    var color = prompt('Is blue her favourite colour?');
+
+    var color = prompt('Is Blue her favourite colour?');
     if (color.toLowerCase() === 'y'|| color.toLowerCase() === 'yes'){
         alert('Yes! She is as cool as Blue');
+        // console.log("Yes, You are correct!");
     } else {
         alert('You are worng');
+        // console.log("No, You are wrong :(");
     }
     console.log(color);
-    console.log(color = 'Blue suits all');
 
     var edu = prompt('Is she an MBA graduate?');
     if (edu.toLowerCase() === 'y'|| edu.toLowerCase() === 'yes'){
         alert('She will be a great business partner!');
+        // console.log("Yes, You are correct!");
     }else {
-        alert('Oh no!');
+        alert('In fact, she did her MBA in Finance and Marketing!');
+        // console.log("No, You are wrong :(");
     }
     console.log(edu);
-    console.log(edu = 'Lets start a business');
+
 
     alert('Thanks for visiting my site ' + username);
 
